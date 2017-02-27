3cd009c37d105063b6b60d23dad90802c8fe44ee
Felix Averlant <felix.averlant@gmail.com>
2017-02-27 12:38:54 -0500

    d3 barchart OK

--- app/src/js/new_game_wizard/d3-barchart.js
+++ app/src/js/new_game_wizard/d3-barchart.js
@@ -10,73 +10,57 @@
                     data: '=data'
                 },
                 link: function(scope, element, attrs) {
+
+                  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+                      width = 200 - margin.left - margin.right,
+                      height = 150 - margin.top - margin.bottom;
+
                     var svg = d3.select(element[0])
                         .append("svg")
-                        .style("width", 200)
-                        .style("height", 200),
-                        margin = {
-                            top: 20,
-                            right: 20,
-                            bottom: 30,
-                            left: 80
-                        },
-                        width = 200,
-                        height = 200;
-
-                    var x = d3.scaleLinear().range([0, 100]);
-                    var y = d3.scaleBand().range([height, 0]);
-
-                    var g = svg.append("g")
+                        .attr("width", width + margin.left + margin.right)
+                        .attr("height", height + margin.top + margin.bottom)
+                        .append("g")
                         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-                    scope.data.sort(function(a, b) {
-                        return a.value - b.value;
-                    });
+                          // set the ranges
+                      var x = d3.scaleBand()
+                                .range([0, width])
+                                .padding(0.1);
+                      var y = d3.scaleLinear()
+                                .range([height, 0]);
 
-                    x.domain([0, d3.max(scope.data, function(d) {
-                        return d.value;
-                    })]);
+                      x.domain(scope.data.map(function(d) { return d.area; }));
+                      y.domain([0, d3.max(scope.data, function(d) { return d.value; })]);
 
-                    y.domain(scope.data.map(function(d) {
-                        return d.area;
-                    })).padding(0.1);
 
-                    g.append("g")
-                        .attr("class", "x axis")
-                        .attr("transform", "translate(0," + height + ")")
-                        .call(d3.axisBottom(x).ticks(5).tickFormat(function(d) {
-                            return parseInt(d);
-                        }).tickSizeInner([-height]));
-
-                    g.append("g")
-                        .attr("class", "y axis")
-                        .call(d3.axisLeft(y));
-
-                    var bar = g.selectAll(".bar")
+                      svg.append("g").attr("id","bar").selectAll(".bar")
                         .data(scope.data)
-                        .enter();
-
-                    bar.append("rect")
+                      .enter().append("rect")
                         .attr("class", "bar")
-                        .attr("x", 0)
-                        .attr("height", 20)
-                        .attr("y", function(d) {
-                            return y(d.area);
-                        })
-                        .attr("width", function(d) {
-                            return x(d.value);
-                        })
-                    bar.append("text")
-                        .attr("class", "label")
-                        .attr("y", function(d) {
-                            return y(d.area) + 20;
-                        })
-                        .attr("x", function(d) {
-                            return x(d.value) - 15;
-                        })
-                        .text(function(d) {
-                            return d.value;
-                        })
+                        .attr("x", function(d) { return x(d.area); })
+                        .attr("width", x.bandwidth())
+                        .attr("y", function(d) { return y(d.value); })
+                        .attr("height", function(d) { return height - y(d.value); });
+
+                        // add the x Axis
+                      svg.append("g")
+                          .attr("transform", "translate(0," + height + ")")
+                          .call(d3.axisBottom(x));
+
+                      // add the y Axis
+                      svg.append("g")
+                          .call(d3.axisLeft(y));
+
+                      svg.select("#bar").selectAll("text")
+                         .data(scope.data)
+                         .enter()
+                         .append('text')
+                         .text(function(d) {return d.value;})
+                         .attr("x", function(d, i) {
+                              return i * (width / scope.data.length) + 15;
+                         })
+                         .attr("y", function(d) { return y(d.value) + 20 })
+                         .attr("fill", "white")
 
                 }
             };
